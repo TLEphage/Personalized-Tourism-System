c7f5de82db3ecb58c256a6fb61c3d44e8762e534
TLEphage <272229503@qq.com>
2025-04-18 01:50:29 +0800

    后端debug

--- BackEnd/run.py
+++ BackEnd/run.py
@@ -27,6 +27,7 @@ class UserResponse(BaseModel):
 class MapRequest(BaseModel):
     start: str  # 起点标识，可以根据实际需要调整数据类型
     end: str    # 终点标识
+    mode: int   # 0:最短路径 1:步行最短时间 2:自行车最短时间 3:电动车最短时间
 
 class Diary(BaseModel):
     username: str
@@ -79,11 +80,13 @@ def get_user(username: str):
 # 最短路径规划接口，使用A*算法
 @app.post("/shortest_path")
 def shortest_path(map_req: MapRequest):
-    path = map_service.a_star(map_req.start, map_req.end)
-    if path:
-        return {"shortest_path": path}
-    else:
+    cost, path = map_service.a_star(map_req.start, map_req.end, map_req.mode)
+    if cost == float('inf'):
         raise HTTPException(status_code=404, detail="未能找到合适的路径")
+    elif map_req.mode == 0:
+        return {"message": f"最短路径长度为 {cost} m", "shortest_path": path}
+    else:
+        return {"message": f"最短时间为 {cost} min", "shortest_path": path}
 
 # 日记存储接口
 @app.post("/diaries")

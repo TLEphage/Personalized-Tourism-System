62493af11a9fd7a36f23289d82e7d91926f6b06f
TLEphage <272229503@qq.com>
2025-05-31 12:30:46 +0800

    上传图片可以自选路径

--- BackEnd/app/routers/upload.py
+++ BackEnd/app/routers/upload.py
@@ -3,12 +3,12 @@ from app.services import upload_service
 
 router = APIRouter(tags=["文件上传"])
 
-@router.post("/image")
-async def upload_image(file: UploadFile = File(...)):
+@router.post("/images{path:path}")
+async def upload_image(file: UploadFile = File(...), path: str = ""):
     # print(file)
     # 验证文件类型
     if not file.content_type.startswith('image/'):
         raise HTTPException(status_code=400, detail="只能上传图片文件")
     contents = await file.read()
-    result = upload_service.upload_image(contents, file.filename)
+    result = upload_service.upload_image(path, contents, file.filename)
     return result
--- BackEnd/app/services/upload_service.py
+++ BackEnd/app/services/upload_service.py
@@ -1,9 +1,13 @@
 import os
 from app.config import IMAGES_DIR
 
-def upload_image(file_contents: bytes, filename: str):
+def upload_image(path: str, file_contents: bytes, filename: str):
     # 保存文件
-    file_path = os.path.join(IMAGES_DIR, filename)
+    path = path.lstrip("/")
+    folder_path = os.path.join(IMAGES_DIR, path)
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+    file_path = os.path.join(folder_path, filename)
     with open(file_path, "wb") as f:
         f.write(file_contents)
     
